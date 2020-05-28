d9da164246f7b0ba9ce0027d1b97b1e7bb6d59c3
pandzia97 <andzia.okarmus@gmial.com>
2020-05-28 12:44:47 +0200

    Gotowy projekt testów TDD

---
+++ src/main/java/main.java
@@ -0,0 +1,39 @@
+import java.io.File;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.util.List;
+
+public class main {
+
+    public static void main(String[] args) {
+
+    }
+
+    public static int czytaniePliku(String slowoLiczone, File plik) throws IOException {
+        if (plik == null) {
+            return 0;
+        }
+
+        if (!plik.exists()) {
+            return 0;
+        }
+        int licznik = 0;
+        List<String> lines = Files.readAllLines(plik.toPath());
+
+        for (String s : lines) {
+            s = s.replace(".", " ").replace(",", " ").replace(":", " ").replace(";", " ").replace("'", " ".replace("?", " ").replace("!", " "));
+
+            String[] words = s.split(" ");
+            for (int i = 0; i < words.length; i++) {
+                if (words[i].equals(slowoLiczone)) {
+
+                    licznik++;
+                }
+            }
+        }
+
+        return licznik;
+    }
+
+}
---
+++ src/test/java/tests.java
@@ -0,0 +1,75 @@
+import com.sun.xml.internal.fastinfoset.tools.FI_DOM_Or_XML_DOM_SAX_SAXEvent;
+import org.junit.Test;
+
+import java.io.File;
+import java.io.IOException;
+import java.nio.file.Paths;
+
+import static org.junit.Assert.*;
+
+public class tests {
+
+    @Test
+    public void fileNull() throws IOException {
+        //given
+        //slowo w pliku null
+        //when
+        int result = main.czytaniePliku("slowo", null);
+
+        //then
+        assertEquals(0, result);
+    }
+    @Test
+    public void nonExistentWordInFile() throws IOException {
+        //given
+        //slowo, ktore nie jest zawarte w istniejacym pliku
+        //when
+        int result = main.czytaniePliku("strona", Paths.get("src", "test", "resources", "plik").toFile());
+        //then
+        assertEquals(0, result);
+    }
+
+    @Test
+    public void wordNull() throws IOException {
+        //given
+        //null zamiast slowa w istniejacym pliku
+        //when
+        int result = main.czytaniePliku(null, Paths.get("src", "test", "resources", "plik").toFile());
+        //then
+        assertEquals(0,result);
+
+    }
+
+    @Test
+    public void wordInTheWrongLocation() throws IOException {
+        //given
+        //jakies slowo i nieistniejacy plik w lokalizacji
+        //when
+        int result = main.czytaniePliku("slowo", Paths.get("src", "test", "resources", "hehe").toFile());
+        //then
+        assertEquals(0, result);
+
+    }
+    @Test
+    public void emptyWord() throws IOException {
+        //given
+        //pusta spacja w istniejacym pliku
+        //when
+        int result = main.czytaniePliku(" ", Paths.get("src","test", "resources", "plik").toAbsolutePath().toFile());
+        //then
+        assertEquals(0, result);
+    }
+
+    @Test
+    public void wordContainedInAnExistingFile() throws IOException {
+        //given
+        //slowo zawarte w istniejacym pliku
+        //when
+        int result = main.czytaniePliku("lorem", Paths.get("src", "test", "resources", "plik").toAbsolutePath().toFile());
+
+        //then
+        assertEquals(3, result);
+
+    }
+
+}
